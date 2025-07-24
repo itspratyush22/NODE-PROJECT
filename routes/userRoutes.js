1b61b6affa19d594baf4ba5634d4db8aeecabcf6
itspratyush22 <112176965+itspratyush22@users.noreply.github.com>
2025-07-25 02:29:19 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../controllers/userController'); // ✅ correct import
+const userController = require('../controllers/userController'); 
 
-router.post('/create', userController.createUser); // ✅ this will now work
+router.post('/create', userController.createUser); 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.get('/:id/notifications', userController.getNotifications);
